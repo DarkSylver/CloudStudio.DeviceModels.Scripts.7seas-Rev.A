cc24c558613132214f89cee3758fada2751a39e9
Cristian Zangaro <cristian.zangaro@cloud.studio>
2025-07-17 03:25:38 -0300

    Update Configuration.js
    Co-authored-by: Cristian Miguel Zangaro <cristian.zangaro@outlook.com>
    Co-authored-by: DarkSylver <darksylvergs@hotmail.com>

---
+++ Configuration.js
@@ -0,0 +1,78 @@
+function getConfiguration(config)
+{
+  // This function allows you to indicate general configuration values
+  // for all devices of this model.
+
+  // Depending on the meaning of the "device address" in this device, 
+  // you may want to change the label that is used to refer to the 
+  // address in the user interface.
+  // For instance, if the address of the device is actually a MAC 
+  // address, you may want to use the code below.
+  
+  config.addressLabel = {en: "DevEUI", es: "DevEUI"};
+}
+
+function getEndpoints(deviceAddress, endpoints)
+{
+  // This function allows you to indicate the initial endpoint configuration
+  // when a device is created using this model. This improves end-user 
+  // experience significantly, because it allows the platform to create
+  // all endpoints included in the device automatically when the device
+  // is created.
+  
+  //Tracker
+    endpoints.addEndpoint("1", "Localización", endpointType.locationTracker);
+    var v = endpoints.addEndpoint("2", "Velocidad", endpointType.genericSensor);
+    v.variableTypeId = 1021;
+    var r = endpoints.addEndpoint("3", "Rumbo", endpointType.genericSensor);
+    r.variableTypeId = 1020;
+    var vm = endpoints.addEndpoint("4", "Variación Magnética", endpointType.genericSensor);
+    vm.variableTypeId = 1022;   
+ 
+}
+
+function validateDeviceAddress(address, result)
+{
+  // Esta función permite validar la dirección de un dispositivo, cuando el usuario 
+  // lo está creando. Si el dispositivo tiene reglas de validación especiales para 
+  // la dirección, pueden verificarse aquí y devolver un mensaje de error en 
+  // caso de que el formato de la dirección sea incorrecto.
+  
+  // En el código siguiente, se realiza una validación para asegurarse de que la 
+  // dirección tiene exactamente 10 caracteres.
+  
+  // if (address.length != 10) {
+  //   result.ok = false;
+  //   result.errorMessage = {
+  //     en: "The address must be 10 characters long", 
+  //     es: "La dirección debe tener exactamente 10 caracteres"
+  //   };
+  // }
+}
+
+function updateDeviceUIRules(device, rules)
+{
+  // Esta función permite especificar reglas de interfaz de usuario en el nivel de 
+  // dispositivo. Por ejemplo, es posible habilitar o deshabilitar la creación 
+  // de endpoints manualmente al dispositivo después de que se creó.
+
+  // En el código siguiente, el agregado manual de endpoints está deshabilitada 
+  // en la interfaz de usuario. Esto significa que el dispositivo se limitará a los 
+  // endpoints definidos por la función getEndpoints() anterior.
+  
+  // rules.canCreateEndpoints = false;
+}
+
+function updateEndpointUIRules(endpoint, rules)
+{
+  // Esta función permite especificar reglas de interfaz de usuario para cada
+  // endpoint del dispositivo. Por ejemplo, es posible habilitar o inhabilitar la
+  // eliminación de endpoints, o la edición de subtipo de endpoint.
+
+  // En el código siguiente, se definen las siguientes reglas:
+  // - Los endpoints no se pueden eliminar.
+  // - El subtipo de endpoint se puede cambiar, pero solo para el segundo endpoint.
+  
+  // rules.canDelete = false;
+  // rules.canEditSubType = (endpoint.address == "2");
+}
